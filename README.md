76427e8a33ae69bb5deee0b3e0f8bfcf286c1e16
Samuli Seppänen <samuli@openvpn.net>
2015-09-11 12:28:57 +0300

    Initial import to GitHub
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # puppet4ify
-Scripts to convert (ERB) files into a format that Puppet 4 accepts
+
+Scripts to convert (ERB) files into a format that Puppet 4 accepts.
+
+# Usage
+
+## puppet4ify_erb.py
+
+Show help:
+
+    $ ./puppet4ify_erb.py -h
+
+Show changes that would be made, using a test file:
+
+    $ ./puppet4ify_erb.py -d --noop test.erb
+
+Create a new file with the corrected content:
+
+    $ ./puppet4ify_erb.py -o outfile.erb infile.erb
+
+Change content of real file in-place:
+
+    $ ./puppet4ify_erb.py infile.erb
+
+# TODO
+
+* Handle array variables / iterators
---
+++ puppet4ify_erb.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# A script to convert ERB templates to a format Puppet 4 accepts. Currently this
+# only converts non-instance variables to instance variables by adding a "@" to
+# the beginning of the variable name as necessary.
+#
+# In debug mode (see -h) changes to be made are printed in a unified diff-like
+# format.
+#
+from shutil import move
+import argparse
+import re
+import sys
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Convert ERB templates into Puppet 4 format')
+    parser.add_argument('infile', nargs=1, type=file, help='file to read from')
+    parser.add_argument('-o', '--outfile', nargs=1, help='file to write to')
+    parser.add_argument('-d', '--debug', action='store_true', help='use debug mode')
+    parser.add_argument('--noop', action='store_true', help='use no-operation mode')
+
+    try:
+        args = parser.parse_args()
+    except IOError:
+        print 'ERROR: Input file could not be opened!'
+        sys.exit(1)
+
+    # Only overwrite the input file if -o is not defined
+    overwrite = False
+    try:
+        if not args.noop:
+            outfile = open(args.outfile[0], 'w')
+    except IOError:
+        print 'ERROR: unable to write to output file!'
+        sys.exit(1)
+    except TypeError:
+        # Outfile was probably not defined, so we write to a temporary name and
+        # overwrite the input file at the end.
+        overwrite = True
+        if not args.noop:
+            outfile = open(args.infile[0].name+'.tmp', 'w')
+
+    # Match non-instance variables (<%= variable %>) in the ERB template and
+    # convert them to instance variables (<%= @variable %>.
+    #
+    # The regular expression captures the entire variable definition and the
+    # variable name inside it, naming the latter as "varname". Then a "@" is
+    # added in front of the variable name if not yet present.
+    p=re.compile(r'(<%=\s?(?P<varname>[\d\w]*?)\s?%>)+?')
+
+    # The args.infile contains an array, even if we only allow one input file
+    for file in args.infile:
+        for line in file:
+            replaced = p.sub('<%= @\g<varname> %>', line)
+            if not line == replaced and args.debug:
+                print '-'+line.rstrip()
+                print '+'+replaced.rstrip()
+            if not args.noop:
+                outfile.write(replaced)
+        file.close()
+
+    # Copy the temporary file over the input file, if no output file is defined
+    if not args.noop:
+        outfile.close()
+        if overwrite:
+            move(outfile.name, args.infile[0].name)
